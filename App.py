ff670dd6d809de781e7b899703f216cccd6d9a7e
Saransh Chakole <chakolesaransh0921@gmail.com>
2025-09-17 13:20:00 +0530

    Update App.py

--- App.py
+++ App.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import seaborn as sb
 
 # Load dataset
-df = pd.read_csv("../DataSets/CARS.csv")
+df = pd.read_csv("CARS.csv")
 
 # Title
 st.title("🚗 Car Horsepower Visualization")
